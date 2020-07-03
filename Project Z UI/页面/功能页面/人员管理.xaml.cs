998d20bf3161d43ba5f7598c760304e924b20c75
rubbish123 <48993179+rubbish123@users.noreply.github.com>
2020-07-03 15:20:36 +0800

    增加了修改与删除成员的后端代码

--- Project Z UI/页面/功能页面/人员管理.xaml.cs
+++ Project Z UI/页面/功能页面/人员管理.xaml.cs
@@ -21,6 +21,25 @@ namespace Project_Z_UI.页面.功能页面
     /// </summary>
     public partial class 人员管理 : Window
     {
+    
+        object cellTempValue = null;
+        object cellFinshEditValue = null;
+        int column;
+        int row;
+        string ID;
+
+        string[] field = new string[] {
+            "编号",
+            "姓名",
+            "年龄",
+            "性别",
+            "密码",
+            "代数",
+            "父亲编号",
+            "是否为管理员",
+            "出生日期"
+        };
+    
         public 人员管理()
         {
             InitializeComponent();
@@ -29,6 +48,121 @@ namespace Project_Z_UI.页面.功能页面
         protected override void OnSourceInitialized(EventArgs e)//去除左上图标
         {
             IconHelper.RemoveIcon(this);
+        }
+        
+        private void DataGrid_BeginningEdit(object sender, DataGridBeginningEditEventArgs e)
+        {
+            //cellTempValue = (e.Column.GetCellContent(e.Row) as TextBlock).Text;
+            //columnName = (e.Column.GetCellContent(0) as TextBlock).Text;
+            //rowID = (DataGrid.Columns[1].GetCellContent(e.Rlock) as TextBox).Text;
+
+            var cells = DataGrid.SelectedCells;
+            row = DataGrid.Items.IndexOf(cells.First().Item);
+            column = DataGrid.CurrentColumn.DisplayIndex;
+            ID = (DataGrid.Columns[1].GetCellContent(DataGrid.Items[row]) as TextBlock).Text;
+            if (column > 0)
+            {
+                cellTempValue = (e.Column.GetCellContent(e.Row) as TextBlock).Text;
+            }
+            //MessageBox.Show(ID);
+        }
+
+        private void DataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+
+            //MessageBox.Show(cellFinshEditValue.ToString());
+            if (column > 0)
+            {
+                cellFinshEditValue = (e.EditingElement as TextBox).Text;
+                if (cellFinshEditValue != cellTempValue)
+                {
+                    string sql = string.Format("update t_user set {0}='{1}' where 编号='{2}'", field[column - 1], cellFinshEditValue, ID);
+                    string connStr = "Data Source=(local);Initial Catalog=family;User ID=sa;Password=qq123456";//连接字符串 
+                                                                                                               //MessageBox.Show(sql);
+                    SqlConnection conn = new SqlConnection(connStr);
+                    conn.Open();
+                    SqlCommand command = new SqlCommand(sql, conn);
+                    //command.CommandType = CommandType.Text;
+                    int s = command.ExecuteNonQuery();
+                    if (s == 0)
+                    {
+                        MessageBox.Show("修改失败");
+                    }
+                    else
+                    {
+                        MessageBox.Show("修改成功");
+                    }
+                    conn.Close();
+                }
+            }
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            string connStr = "Data Source=(local);Initial Catalog=family;User ID=sa;Password=qq123456";
+            string sql = string.Format("delete from t_user where 编号='{0}'", ID);
+            //MessageBox.Show(sql);
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            SqlCommand command = new SqlCommand(sql, conn);
+            //command.CommandType = CommandType.Text;
+            int s = command.ExecuteNonQuery();
+            if (s == 0)
+            {
+                MessageBox.Show("删除失败");
+            }
+            else
+            {
+                MessageBox.Show("删除成功");
+            }
+            string sql1 = "select * from t_user";
+            SqlDataAdapter sqlada = new SqlDataAdapter(sql1, conn);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            sqlada.Fill(ds, "基本成员表");
+            DataView dv = new DataView(ds.Tables["基本成员表"]);
+            DataGrid.ItemsSource = dv;
+            conn.Close();
+        }
+
+        private void searchName(object sender, RoutedEventArgs e)
+        {
+            string sql = "Data Source=(local);Initial Catalog=family;User ID=sa;Password=qq123456";//连接字符串 
+            SqlConnection sqlcon = new SqlConnection(sql);// 
+            string sql1 = "select * from t_user where 姓名='" + textbox1.Text + "'";
+            SqlDataAdapter sqlada = new SqlDataAdapter(sql1, sqlcon);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            sqlada.Fill(ds, "基本成员表");
+            DataView dv = new DataView(ds.Tables["基本成员表"]);
+            DataGrid.ItemsSource = dv;
+        }
+
+        private void searchID(object sender, RoutedEventArgs e)
+        {
+            string sql = "Data Source=(local);Initial Catalog=family;User ID=sa;Password=qq123456";//连接字符串 
+            SqlConnection sqlcon = new SqlConnection(sql);// 
+            string sql1 = "select * from t_user where 编号='" + textbox2.Text + "'";
+            SqlDataAdapter sqlada = new SqlDataAdapter(sql1, sqlcon);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            sqlada.Fill(ds, "基本成员表");
+            DataView dv = new DataView(ds.Tables["基本成员表"]);
+            DataGrid.ItemsSource = dv;
+        }
+
+        private void searchAll(object sender, RoutedEventArgs e)
+        {
+            string sql = "Data Source=(local);Initial Catalog=family;User ID=sa;Password=qq123456";//连接字符串 
+            SqlConnection sqlcon = new SqlConnection(sql);// 
+            string sql1 = "select * from t_user";
+            SqlDataAdapter sqlada = new SqlDataAdapter(sql1, sqlcon);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            sqlada.Fill(ds, "基本成员表");
+            DataView dv = new DataView(ds.Tables["基本成员表"]);
+            DataGrid.ItemsSource = dv;
         }
     }
 }
